7aeac1c914ace33bb4e2ccddd6fbfd462c5eed94
DarkWaterRabbit <dima.kyz16@yandex.ru>
2024-12-05 21:05:22 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Дмитрий"
+print('name:',name)
+age = 31
+print('age:',age)
+age = 90
+print('New Age:',age)
+is_student = True
+print('is_student:',is_student)
---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+col_dz = 12
+
+col_hours = 1.5
+
+kyrs = 'Python'
+
+time_task = col_hours/col_dz
+
+print('Курс:',kyrs,',всего задач:',col_dz,',затрачено часов:',col_hours,',среднее время выполнения:',time_task,',часа.' )
