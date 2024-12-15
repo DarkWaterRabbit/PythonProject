53c5ea508b7302cd99f69ab14780fb0507036d5b
DarkWaterRabbit <dima.kyz16@yandex.ru>
2024-12-15 15:07:33 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,17 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+
+D = (len(my_list))
+
+while i != D :
+     if my_list[i] > 0 and my_list[i] != 0:
+
+        print(my_list[i])
+        i += 1
+
+     else:
+        i += 1
+
+print('Конец цикла')
+
+
